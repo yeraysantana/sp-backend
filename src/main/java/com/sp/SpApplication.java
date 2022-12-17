1d2fdd6650067a379dab3d973193428237bcd052
yeray.santana <yeray.santana@sendizer.com>
2022-12-17 23:20:50 +0000

    First commit

---
+++ src/main/java/com/sp/SpApplication.java
@@ -0,0 +1,13 @@
+package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpApplication.class, args);
+	}
+
+}
